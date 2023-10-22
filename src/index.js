c6b35e4a86a3e9aff82849d6309579c74a9b8e70
Shivam <shivamshuklass3277@gmail.com>
2023-10-23 02:13:36 +0530

    rm li

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { registerLicense } from "@syncfusion/ej2-base";
-registerLicense(
-  "Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1JpR2JGfV5yd0VEal1UTnNdUj0eQnxTdEZiWX1ccXBUQ2VfVU1+XA=="
-);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
